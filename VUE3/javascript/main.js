d174dc5abc027752b4235202fa0bd867362aa308
Taiga <81619089+tiger3046@users.noreply.github.com>
2022-12-21 14:34:44 +0900

    Add files via upload

---
+++ VUE3/javascript/main.js
@@ -0,0 +1,24 @@
+const app=Vue.createApp({
+    data:() => ({
+        newItem:'',
+        todos:[]
+    }),
+    methods:{
+        addItem:function(event){
+            if(this.newItem ===''){
+                alert('タスクを入力して下さい。')
+                return
+            }
+            let todo = {
+                item:this.newItem,
+                isDone:false
+            }
+            this.todos.push(todo)
+            this.newItem = ''
+        },
+        deleteItem:function(index){
+            this.todos.splice(index ,1)
+        }
+    }
+})
+app.mount('#app')
